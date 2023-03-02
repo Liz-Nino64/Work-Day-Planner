dda453dcc5c6abfc68ad0b07ac20d1e5bacefb9a
Liz <elizabethnino813@Elizabeths-MacBook-Pro.local>
2023-03-01 23:56:24 -0600

    Fixed Javascript file to allow better ufnctionality

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 console.log("Javascript works!!")
-var saveButtonEl1 = $("#hour-9")
-var saveButtonEl2 = $("#hour-10")
-var saveButtonEl3 = $("#hour-11")
-var saveButtonEl4 = $("#hour-12")
-var saveButtonEl5 = $("#hour-1")
-var saveButtonEl6 = $("#hour-2")
-var saveButtonEl7 = $("#hour-3")
-var saveButtonEl8 = $("#hour-4")
-var saveButtonEl9 = $("#hour-5")
-var userInputEl = $("#textarea")
-var storedInput = localStorage.getItem("textarea")
+var saveButtonEl1 = $("#hour-9").children("button")
+var saveButtonEl2 = $("#hour-10").children("button")
+var saveButtonEl3 = $("#hour-11").children("button")
+var saveButtonEl4 = $("#hour-12").children("button")
+var saveButtonEl5 = $("#hour-1").children("button")
+var saveButtonEl6 = $("#hour-2").children("button")
+var saveButtonEl7 = $("#hour-3").children("button")
+var saveButtonEl8 = $("#hour-4").children("button")
+var saveButtonEl9 = $("#hour-5").children("button")
+var textArea9 = $("#text-9")
+var textArea10 = $("#text-10")
+var textArea11 = $("#text-11")
+var textArea12 = $("#text-12")
+var textArea1 = $("#text-1")
+var textArea2 = $("#text-2")
+var textArea3 = $("#text-3")
+var textArea4 = $("#text-4")
+var textArea5 = $("#text-5")
 
 $(function () {
   jQuery(document).ready(function($) {
@@ -21,40 +28,39 @@ $(function () {
 
   saveButtonEl1.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
-
+    localStorage.setItem("text-9", textArea9.val())
   });
   saveButtonEl2.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
-    });
+    localStorage.setItem("text-10", textArea10.val())
+  });
   saveButtonEl3.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
+    localStorage.setItem("text-11", textArea11.val())
     });
   saveButtonEl4.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
+    localStorage.setItem("text-12", textArea12.val())
     });
   saveButtonEl5.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
+    localStorage.setItem("text-1", textArea1.val())
     });
   saveButtonEl6.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
+    localStorage.setItem("text-2", textArea2.val())
     });
   saveButtonEl7.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
+    localStorage.setItem("text-3", textArea3.val())
     });
   saveButtonEl8.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
+    localStorage.setItem("text-4", textArea4.val())
     });
   saveButtonEl9.on("click", function () {
     console.log("Saved!!")
-    localStorage.setItem("textarea", userInputEl);
+    localStorage.setItem("text-5", textArea5.val())
     });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
